b14bf223555703d75de4d9a7d755c46363613178
pablolx <pablo.pinheiro@gmail.com>
2025-10-17 10:35:15 -0300

    Removendo chave.json do controle de versão

--- portfolio2025/settings.py
+++ portfolio2025/settings.py
@@ -102,8 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website/static')]
-
+STATICFILES_DIRS = [
+    BASE_DIR / "apps/website/static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
