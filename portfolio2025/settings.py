1d8205d631c8edc4ccee366079bd9aef43e46a6c
pablolx <pablo.pinheiro@gmail.com>
2025-10-17 17:45:27 -0300

    Limpeza de cache para aplicar .gitignore

--- portfolio2025/settings.py
+++ portfolio2025/settings.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps', 'website', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps', 'website', 'static', 'images')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
