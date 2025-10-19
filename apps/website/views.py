b244768a65bea28d5bc37e893dd0865be74f8b41
pablolx <pablo.pinheiro@gmail.com>
2025-10-19 10:52:32 -0300

    Adicionando estilo para a foto de perfil no style.css

--- apps/website/views.py
+++ apps/website/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+import requests
 from .models import Project
 
 def index(request):
     projects = Project.objects.order_by("-created_at")
     return render(request, "index.html", {"projects": projects})
+
+
+
+def portfolio(request):
+    github_user = "SEU_USUARIO_GITHUB"
+    response = requests.get(f"https://api.github.com/users/{github_user}/repos")
+    repos = response.json() if response.status_code == 200 else []
+
+    context = {
+        "repos": repos,
+    }
+    return render(request, "portfolio.html", context)
