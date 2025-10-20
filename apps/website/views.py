efa4477e3fa83f9e4b979f5bacbccfe7380ae973
pablolx <pablo.pinheiro@gmail.com>
2025-10-20 09:36:23 -0300

    Update index route to point to portfolio view

--- apps/website/views.py
+++ apps/website/views.py
@@ -9,11 +9,5 @@ def index(request):
 
 
 def portfolio(request):
-    github_user = "SEU_USUARIO_GITHUB"
-    response = requests.get(f"https://api.github.com/users/{github_user}/repos")
-    repos = response.json() if response.status_code == 200 else []
-
-    context = {
-        "repos": repos,
-    }
-    return render(request, "index.html", context)
+    projetos = Project.objects.all()
+    return render(request, "index.html", {"projetos": projetos})
